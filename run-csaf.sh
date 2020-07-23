9cd8b06914cb4801224994e90ac6ea25699c767f
Matt Bauer <mbauer@galois.com>
2020-07-23 12:28:21 -0400

    distinguish between local and Docker Hub runs

--- run-csaf.sh
+++ run-csaf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+LOCAL=0
 SCRIPT_DIR="csaf_architecture"
 
 validate_dir() {
@@ -18,13 +19,17 @@ source .common.sh
 
 print_help() {
 	printf "Usage: -t <tag_name>\n"
+	printf "   -l      build the image locally\n"
 	printf "   -t      the tag of the image { stable, edge, latest }\n"
 	printf "   -h      prints the help menu\n"
 	printf "\n"
 }
 
-while getopts ":t:h" opt; do
+while getopts ":t:lh" opt; do
 	case ${opt} in
+        l )
+		LOCAL=1
+		;;
 	t )
 		IMAGE_TAG=$OPTARG
 		;;
@@ -38,7 +43,12 @@ while getopts ":t:h" opt; do
 	esac
 done
 
-build_img
+if [[ ${LOCAL} -eq 1 ]] ; then
+	build_img
+else
+	show_info "Pulling image from Docker Hub"
+	# TODO
+fi
 
 docker run --network host ${IMAGE_NAME}:${IMAGE_TAG}
 
