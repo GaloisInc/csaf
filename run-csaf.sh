7b1bc711b6e723ec82f88eac130a3b9fb89d69d9
Matt Bauer <mbauer@galois.com>
2020-07-23 12:18:07 -0400

    polish the csaf run script

--- run-csaf.sh
+++ run-csaf.sh
@@ -16,8 +16,31 @@ validate_dir
 
 source .common.sh
 
+print_help() {
+	printf "Usage: -t <tag_name>\n"
+	printf "   -t      the tag of the image { stable, edge, latest }\n"
+	printf "   -h      prints the help menu\n"
+	printf "\n"
+}
+
+while getopts ":t:h" opt; do
+	case ${opt} in
+	t )
+		IMAGE_TAG=$OPTARG
+		;;
+	h )
+		print_help
+		exit 0
+		;;
+	* )
+		show_error_and_exit "Unknown argument"
+		;;
+	esac
+done
+
 build_img
 
 docker run --network host ${IMAGE_NAME}:${IMAGE_TAG}
 
+# TODO
 #docker run --network host -it -v $PWD:/beat-pd-pdx -v $BEAT_PD_DIR:/datasets/beat-pd $IMAGE_NAME:$IMAGE_TAG
