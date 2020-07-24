3021814a85a278651f17700bf5b7e7d555e3e13b
Ethan Lew <elew@galois.com>
2020-07-24 13:06:14 -0700

    update scripts to work in new structure

--- src/scripts/run_system.py
+++ src/scripts/run_system.py
@@ -1,7 +1,7 @@
 import csaf.system as csys
 import csaf.config as cconf
 
-my_system = csys.System.from_toml("../examples/config/config.toml")
+my_system = csys.System.from_toml("../../examples/config/config.toml")
 my_system.activate_system()
 
 idx = 0
