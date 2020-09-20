9977d346a766950f2a3bfe86e08f4745bf38a8bd
Michal Podhradsky <mpodhradsky@galois.com>
2020-09-19 17:10:23 -0700

    Improve user experience with run-csaf, gate analysis tool

--- run-csaf.sh
+++ run-csaf.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-LOCAL=0
-NATIVE=0
-CSAF_LOC=""
-CONFIG_NAME=""
-JOB_CONFIG_PATH=""
-
+JUPYTER=0
 source .common.sh
 
 validate_dir
 
 print_help() {
-	printf "Usage:\n"
+	printf "\033[1mCSAF\033[0m\n"
+    printf "    Control System Analysis Framework (CSAF) is a middleware framework that
+    makes creating and evaluating control systems as simple as possible. Control
+    loop topologies and component implementations are specified independently of
+    the middleware.\n"
+    printf "\n\033[1mUSAGE\033[0m\n"
+	printf "   -e      the name of the example { f16-shield, f16-simple, inv-pendulum }\n"
 	printf "   -c      the name of the config file\n"
 	printf "   -d      fully qualified path to the directory defining the control system\n"
-	printf "   -f      fully qualified path to the job config file\n"
+	printf "   -f      name of the job config file (must be in the same directory as your system)\n"
 	printf "   -j      launch a jupyter notebook\n"
 	printf "   -l      build the image locally\n"
 	printf "   -n      run CSAF natively\n"
 	printf "   -t      the tag of the image { stable, edge, latest }\n"
 	printf "   -h      prints the help menu\n"
-	printf "\n"
+	printf "   -x      clear the output for a particular example/config"
+	printf "\n\n"
+    printf "\033[1mEXAMPLES\033[0m\n"
+    printf "Run f16-simple example:\n"
+    printf "    ./run-csaf.sh -e f16-simple\n"
+    printf "Run f16-simple example natively (not in a docker container):\n"
+    printf "    ./run-csaf.sh -e f16-simple -n\n"
+    printf "Start a jupyter notebook with f16 example:\n"
+    printf "    ./run-csaf.sh -e f16-simple -j\n"
+    printf "    ./run-csaf.sh -e f16-shield -j\n"
+    printf "    ./run-csaf.sh -d \${PWD}/examples/f16 -j\n"
+    printf "Start jupyter notebook with your own example:\n"
+    printf "    ./run-csaf.sh -j -d \${PWD}/examples/inverted-pendulum\n"
+    printf "Run f16-shield with your own job config:\n"
+    printf "    ./run-csaf.sh -e f16-shield -f f16_job_conf.toml\n"
+    printf "Clear generated outputs for f16 example:\n"
+    printf "    ./run-csaf.sh -e f16-simple -x\n"
 }
 
-while getopts ":c:d:f:t:jlhn" opt; do
+while getopts ":c:d:e:f:t:jlhn:x" opt; do
 	case ${opt} in
         c )
 		CONFIG_NAME=$OPTARG
@@ -30,11 +47,14 @@ while getopts ":c:d:f:t:jlhn" opt; do
         d )
 		CSAF_LOC=$OPTARG
 		;;
+		e )
+		EXAMPLE_NAME=$OPTARG
+		;;
 		f )
 		JOB_CONFIG_PATH=$OPTARG
 		;;
         j )
-		JUYPTER=1
+		JUPYTER=1
 		;;
         l )
 		LOCAL=1
@@ -45,6 +65,9 @@ while getopts ":c:d:f:t:jlhn" opt; do
 		t )
 		IMAGE_TAG=$OPTARG
 		;;
+        x )
+        CLEAR_OUTPUT=1
+        ;;
 		h )
 		print_help
 		exit 0
@@ -55,7 +78,34 @@ while getopts ":c:d:f:t:jlhn" opt; do
 	esac
 done
 
-if [ -z ${CONFIG_NAME} ]
+if [[ -n ${EXAMPLE_NAME} && -n ${CONFIG_NAME} ]] ;
+then
+	show_error_and_exit "A CSAF config file and an example name supplied at the same time. Please choose only one."
+fi
+
+if [ ! -z ${EXAMPLE_NAME} ] ;
+then
+    case $EXAMPLE_NAME in
+        "f16-simple")
+            CONFIG_NAME="f16_simple_config.toml"
+            CSAF_LOC=${PWD}/"examples/f16"
+            ;;
+        "f16-shield")
+            CONFIG_NAME="f16_shield_config.toml"
+            CSAF_LOC=${PWD}/"examples/f16"
+            ;;
+        "inv-pendulum")
+            CONFIG_NAME="inv_pendulum_config.toml"
+            CSAF_LOC=${PWD}/"examples/inverted-pendulum"
+            ;;
+        *)
+            show_error_and_exit "Unknown example: ${EXAMPLE_NAME} Please use one of [f16-shield, f16-simple, inv-pendulum]"
+            ;;
+    esac
+fi
+
+
+if [[ ${JUPYTER} -eq 0 && -z ${CONFIG_NAME} ]] ;
 then
 	show_error_and_exit "A CSAF config file must be supplied"
 fi
@@ -69,7 +119,7 @@ if [[ ${LOCAL} -eq 1 && ${NATIVE} -eq 1 ]] ; then
 	show_error_and_exit "the \'native\' and \'local\' options cannot be combined"
 fi
 
-if [[ ${JUYPTER} -eq 1 && ${NATIVE} -eq 1  ]] ; then
+if [[ ${JUPYTER} -eq 1 && ${NATIVE} -eq 1  ]] ; then
     show_error_and_exit "the \'native\' and \'jupyter\' options cannot be combined"
 fi
 
@@ -78,14 +128,27 @@ if [[ ${LOCAL} -eq 1 ]] ; then
 	build_img
 fi
 
+if [[ ${CLEAR_OUTPUT} -eq 1 ]] ; then
+    echo "Remove ${CSAF_LOC}/output and ${CSAF_LOC}/codec? [Y/n]"
+    read ACK
+    if [ ${ACK} = "Y" ]; then
+        rm -rf ${CSAF_LOC}/output
+        rm -rf ${CSAF_LOC}/codec
+        echo "Done"
+    else
+        echo "Aborting."
+    fi
+    exit 0
+fi
+
 if [[ ${NATIVE} -eq 1 ]] ; then
-	if [[ ${JUYPTER} -eq 1 ]] ; then
+	if [[ ${JUPYTER} -eq 1 ]] ; then
 		jupyter notebook --no-browser --notebook-dir=${PWD}/docs/notebooks
 	else
 		python3 "src/run_system.py" ${CSAF_LOC} ${CONFIG_NAME} ${JOB_CONFIG_PATH}
 	fi
 else
-	if [[ ${JUYPTER} -eq 1 ]] ; then
+	if [[ ${JUPYTER} -eq 1 ]] ; then
 		docker run --init --network host -it -v ${CSAF_LOC}:/csaf-system \
 			-v ${PWD}/src:/app -v ${PWD}/docs/notebooks:/notebooks \
 			${IMAGE_NAME}:${IMAGE_TAG} "jupyter" "notebook" "--port=8888" \
--- src/run_system.py
+++ src/run_system.py
@@ -31,7 +31,8 @@ model_conf = cconf.SystemConfig.from_toml(config_filename)
 
 # Optional job configuration
 if len(sys.argv) > 3:
-    with open(sys.argv[3], 'r') as f:
+    job_filename = csaf_dir + "/" + sys.argv[3]
+    with open(job_filename, 'r') as f:
         job_conf = toml.load(f)
 else:
     job_conf = {}
