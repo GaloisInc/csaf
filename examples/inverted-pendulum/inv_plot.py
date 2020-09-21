a0aa90584ce0aa1594ab3aabdd8337943f09c998
Michal Podhradsky <mpodhradsky@galois.com>
2020-09-21 16:34:54 -0700

    Fix state description for inverted pendulum

--- examples/inverted-pendulum/inv_plot.py
+++ examples/inverted-pendulum/inv_plot.py
@@ -8,9 +8,9 @@ def plot_pendulum(trajs):
     """
     fig, ax = plt.subplots(figsize=(12, 8), nrows=2, ncols=2, sharex=True)
     ax[0][0].set_title("Pendulum Plant")
-    plot_component(ax[0][0], trajs, "plant", "states", 0, "position (m)", do_integrate=False)
+    plot_component(ax[0][0], trajs, "plant", "states", 0, "position (m)")
     plot_component(ax[0][0], trajs, "maneuver", "outputs", 0, "Setpoint ()", mult=-1.0)
-    plot_component(ax[1][0], trajs, "plant", "states", 2, "angle (rad)", do_integrate=True, fix=False)
+    plot_component(ax[1][0], trajs, "plant", "states", 2, "angle (rad)")
     ax[1][0].set_xlabel("Time (s)")
 
     ax[0][1].set_title("Controller")
