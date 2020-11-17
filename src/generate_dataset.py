b8589a1f3b6506b10ac8dc55dd15921c679b1fb5
Ethan Lew <elew@galois.com>
2020-11-17 10:11:06 -0800

    add docstrings, terminating conditions

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -1,9 +1,56 @@
+#!/usr/bin/python3
 """ Dataset Creator
 
 @author: Ethan Lew <elew@galois.com>
 08/03/2020
 
 Simulates systems based on user supplied initial conditions
+
+usage: generate_dataset.py [-h] [--ic-file IC_FILE]
+                           [--data-format DATA_FORMAT]
+                           [--config-file CONFIG_FILE]
+                           [--output-file OUTPUT_FILE] [--component COMPONENT]
+                           [--t-max T_MAX]
+
+TODO: the fixed terminating conditions makes this work for the f16 systems only.
+Make this configurable.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --ic-file IC_FILE     Path to Initial Conditions JSON
+  --data-format DATA_FORMAT
+                        Dataset Formats (default|openai)
+  --config-file CONFIG_FILE
+                        Path to CSAF System TOML
+  --output-file OUTPUT_FILE
+                        Name of Output JSON File to Save
+  --component COMPONENT
+                        Name of Component to Collect
+  --t-max T_MAX         Maximum time to simulate each run
+
+Example Usage:
+    # in /src
+    python generate_dataset.py --ic-file=ic.json --config-file=../examples/f16/f16_simple_config.toml
+
+Specify Initial Conditions to Simulate:
+    IC (Initial Conditions) JSON format:
+        // list of tables, mapping component identifiers to initial condition arrays
+        [
+            {"componentname": [...]},
+            ...
+            {"componentname": [...]},
+            {"componentname" :[...]}
+        ]
+
+    Example  F16 <ic.json>:
+    [
+        {"plant": [532.2267341599213, 0.037027160081059704,
+                    0.0, 0.7853981633974483, 3.0113293881657786,
+                    -0.7427364757763312, 0.0, -0.436285343076811,
+                    0.0, 0.0, 0.0,
+                    4324.831502507831, 9.0]
+        }
+    ]
 """
 import argparse
 import os
@@ -18,7 +65,6 @@ import csaf.config as cconf
 import csaf.system as csys
 import csaf.trace as ctr
 
-tspan: list = [0.0, 5.0]
 RunsType = typ.List[typ.Union[typ.Tuple[bool, ctr.TimeTrace], Exception]]
 
 
@@ -78,6 +124,11 @@ def fmt_parser(v: str) -> str:
     return v
 
 
+def ground_collision_condition(cname, outs):
+    """ground collision premature termnation condition"""
+    return cname == "plant" and outs["states"][11] <= 0.0
+
+
 # parse user specified options
 fmts = ["default", "openai"]
 
@@ -102,6 +153,10 @@ parser.add_argument("--component",
                     type=str,
                     help=f"Name of Component to Collect",
                     default="autopilot")
+parser.add_argument("--t-max",
+                    type=int,
+                    help=f"Maximum time to simulate each run",
+                    default=35)
 args = parser.parse_args()
 
 # resolve filepaths and ensure that they exist
@@ -120,8 +175,14 @@ model_conf = cconf.SystemConfig.from_toml(config_file)
 with open(ic_file, 'r') as fp:
     ic_states: list = json.load(fp)
 
+tspan: list = [0.0, args.t_max]
 n_tasks: int = len(ic_states)
-runs: RunsType = run_workgroup(n_tasks, model_conf, ic_states, tspan)
+runs: RunsType = run_workgroup(
+    n_tasks,
+    model_conf,
+    ic_states,
+    tspan,
+    terminating_conditions=ground_collision_condition)
 
 # output to JSON file
 if args.data_format == "default":
