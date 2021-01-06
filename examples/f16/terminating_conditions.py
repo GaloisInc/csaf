e383778ff4dd78dfe46f098d56614f1dc1f455a4
Ethan Lew <elew@galois.com>
2021-01-06 13:26:34 -0800

    add initial Bayesian falsifier implementation

--- examples/f16/terminating_conditions.py
+++ examples/f16/terminating_conditions.py
@@ -3,3 +3,33 @@
 def ground_collision(cname, outs) -> bool:
         """ground collision"""
         return cname == "plant" and outs["states"][11] <= 0.0
+
+
+def reward_func(trajs):
+        import numpy as np
+
+        altitude_min = 0  # ft AGL
+        altitude_max = 4000  # ft AGL 45000
+        nz_max = 15  # G's original is 9
+        nz_min = -3  # G's original is -2
+        # ps_max_accel_deg = 500  # /s/s
+
+        v_min = 300  # ft/s
+        v_max = 2500  # ft/s
+        alpha_min_deg = -10  # deg
+        alpha_max_deg = 45  # deg
+        beta_max_deg = 30  # deg
+
+        # did not consider the change rate of ps here
+        constraints_dim = [0, 1, 2, 11, 13]
+        constraints_box = np.array([[v_min, alpha_min_deg, -beta_max_deg, altitude_min, nz_min]
+                                           , [v_max, alpha_max_deg, beta_max_deg, altitude_max, nz_max]])
+
+        states = np.hstack((np.array(trajs["plant"].states), np.array(trajs["plant"].outputs)))
+        dist_to_lb = np.abs(states[:, constraints_dim] - constraints_box[0])
+        dist_to_ub = np.abs(states[:, constraints_dim] - constraints_box[1])
+
+        min_dist = np.min(np.array([dist_to_ub, dist_to_lb]), axis=0)
+        norm_min_dist = min_dist / (constraints_box[1] - constraints_box[0])
+
+        return np.mean(norm_min_dist)
--- src/tests_static.py
+++ src/tests_static.py
@@ -97,6 +97,34 @@ class RunSystemParallelTest(cst.SystemTest):
         return runs, passed_termcond
 
 
+def attack(func,
+           space,
+           acq_func="EI",
+           n_calls=5,
+           n_random_starts=2):
+    """
+    Attack with BO
+    :param func: evaluated function with a collector callback
+    :param space: lower and upper boundary
+    :param acq_func: BO acquisition function
+    :param n_calls: the number of evaluations of f
+    :param n_random_starts: the number of random initialization points
+    :return: unsafe initial states
+    """
+    from skopt import gp_minimize
+    dimensions = np.array(space).T
+    dimensions[:, 0] -= 1e-5
+
+    gp_minimize(func, dimensions,
+                acq_func=acq_func,
+                n_calls=n_calls,
+                n_random_starts=n_random_starts,
+                noise=0,
+                n_jobs=1)
+
+    return func.get_collector()
+
+
 class StaticRunTest(RunSystemParallelTest):
     """test that evaluates traces from system simulations"""
     valid_fields = ["iterations",
@@ -152,15 +180,44 @@ class BayesianFalsifierTest(RunSystemTest):
     """test attempts to falsify a condition using bayesian optimization"""
     valid_fields = ["n_calls",
                     "initial_conditions",
+                    "bounds",
+                    "terminating_conditions",
+                    "reward_fcn",
                     "show_status",
                     "tspan"]
 
     required_fields = ["n_calls",
-                    "initial_conditions",
-                    "show_status",
-                    "tspan"]
+                       "initial_conditions",
+                       "reward_fcn",
+                       "bounds",
+                       "terminating_conditions",
+                       "show_status",
+                       "tspan"]
+
+    @_("reward_fcn")
+    def _(self, fcn_name):
+        import pathlib, sys
+        if fcn_name is None:
+            return None
+        #FIXME: hard-coded
+        pypath = str(pathlib.Path(self.base_dir) / "terminating_conditions.py")
+        mod_path = str(pathlib.Path(pypath).parent.resolve())
+        if mod_path not in sys.path:
+            sys.path.insert(0, mod_path)
+        # TODO: this is hard-coded
+        return getattr(__import__("terminating_conditions"), fcn_name)
 
     def execute(self, system_conf):
-        pass
+        def simulate(initial_conditions):
+            system = csys.System.from_config(system_conf)
+            x0 = initial_conditions
+            if x0:
+                # TODO: bad hard coding
+                system.set_state("plant", x0)
+            trajs, passed = system.simulate_tspan(self.tspan, show_status=self.show_status, return_passed = True, terminating_conditions=self.terminating_conditions)
+            system.unbind()
+            return self.reward_fcn(trajs)
+        ret = attack(simulate, list(zip(*[b[:-1] for b in self.bounds])))
+
 
 
