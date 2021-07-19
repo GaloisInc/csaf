f8e3f6b4e0d92e5b55703b96124f1e76b34bbd6e
Michal Podhradsky <mpodhradsky@galois.com>
2021-07-19 11:43:44 -0700

    Fold notebook testing into actions

---
+++ .test_notebooks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+FILES=`find .  -name "*.ipynb" ! -name '*checkpoint*'`
+do
+    echo ">>> Processing ${NOTEBOOK}"
+    BASENAME=${NOTEBOOK%.*}
+    PYTHONFILE=${BASENAME}".py"
+    JOBNAME=`basename ${BASENAME}`
+    if [[ $NOTEBOOK =~ .*f16.* ]] || [[ $NOTEBOOK =~ .*csaf_env_example.* ]]; then
+      EXAMPLE="f16"
+    else
+    if [[ $NOTEBOOK =~ .*cansat.* ]]; then
+      EXAMPLE="cansat"
+    else
+    if [[ $NOTEBOOK =~ .*dubins.* ]]; then
+      EXAMPLE="rejoin"
+    else
+      echo "Unknown example!"
+      exit 1
+    fi
+    fi
+    fi
+    # Now actually execute the commands
+    echo ">>> Adjust example paths"
+    ln -s ${PWD}/examples/${EXAMPLE} ./csaf-system
+    echo ">>> Testing ${NOTEBOOK}"
+    jupyter nbconvert --to python $NOTEBOOK
+    ipython $PYTHONFILE
+    echo ">>> Testing ${NOTEBOOK} complete!"
+    rm -rf ./csaf-system
+done
