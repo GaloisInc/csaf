0be0881772e7c6a6c993492a9916ffc0ee1964dc
Ethan Lew <elew@galois.com>
2021-12-22 16:49:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="csaf",
-    version="0.0.1",
+    version="0.2",
     author=["Ethan Lew", "Michal Podhradsky", "Aditya Zutshi"],
     author_email=["ethanlew16@gmail.com", "mpodhradsky@galois.com", "aditya.zutshi@galois.com"],
     description="Control Systems Analysis Framework",
